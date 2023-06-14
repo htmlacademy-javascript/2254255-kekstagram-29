6c2655319742c48820d8712b289587560176877f
Andrey Matveev <andrei_matveev1250@mail.ru>
2023-06-14 11:00:54 +0600

    Пишет комментарии по стандарту DocStrings

--- js/functions.js
+++ js/functions.js
@@ -1,27 +1,44 @@
-// Функция для проверки длины строки.
+/**
+ * Функция для проверки длины строки.
+ * @param {string} string - Проверяемая строка
+ * @param {number} maxLength - Максимальная длина
+ * @return {boolean} - Проверяет, что длина строки меньше, либо равна максимальной длине
+ */
 function lengthCheck (string, maxLength) {
   return string.length <= maxLength;
 }
 
-// Функция для проверки, является ли строка палиндромом.
+lengthCheck('Функция для проверки длины строки', 21);
+
+/**
+ * Функция для проверки, является ли строка палиндромом.
+ * @param {string} phrase - Проверяемая строка
+ * @return {boolean} - Проверяет, что что строка является палиндромом (обратный порядок символов совпадает с прямым)
+ */
 function palindromeCheck (phrase) {
-  const normalise = phrase.toLowerCase().replaceAll(' ', '');
+  const normalize = phrase.toLowerCase().replaceAll(' ', '');
   let reversed = '';
 
-  for (let i = 1; i <= normalise.length; i++) {
-    reversed += normalise.at(-i);
+  for (let i = 1; i <= normalize.length; i++) {
+    reversed += normalize.at(-i);
   }
 
-  return normalise === reversed;
+  return normalize === reversed;
 }
 
-// Функция принимает строку, извлекает содержащиеся в ней цифры от 0 до 9 и возвращает их в виде целого положительного числа.
-function numberExtraction (sourse) {
-  const normalise = sourse.toString().replaceAll(' ', '');
+palindromeCheck('Функция для проверки, является ли строка палиндромом');
+
+/**
+ * Функция принимает строку, извлекает содержащиеся в ней цифры от 0 до 9 и возвращает их в виде целого положительного числа.
+ * @param {string || number} source - Проверяемая строка
+ * @return {boolean} - Извлекает из строки цифры и возвращает их в виде целого положительного числа
+ */
+function numberExtraction (source) {
+  const normalize = source.toString().replaceAll(' ', '');
   let result = '';
 
-  for (let i = 0; i < normalise.length; i++) {
-    const symbol = Number(normalise[i]);
+  for (let i = 0; i < normalize.length; i++) {
+    const symbol = Number(normalize[i]);
     if (Number.isNaN(symbol) === false) {
       result += symbol;
     }
@@ -29,3 +46,5 @@ function numberExtraction (sourse) {
 
   return result;
 }
+
+numberExtraction('Функция принимает строку, извлекает содержащиеся в ней цифры от 0 до 9 и возвращает их в виде целого положительного числа');
