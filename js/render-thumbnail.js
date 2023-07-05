61e5ed00536e8e64298e687f283e70ca274ec361
Andrey Matveev <andrei_matveev1250@mail.ru>
2023-07-05 11:47:16 +0600

    Вносит правки по 7 модулю

---
+++ js/render-thumbnail.js
@@ -0,0 +1,23 @@
+const thumbnailTemplate = document.querySelector('#picture')
+  .content
+  .querySelector('.picture');
+const thumbnailContainer = document.querySelector('.pictures');
+
+/**
+ * Функция для отрисовки миниатюр.
+ */
+function renderThumbnails(createThumbnails) {
+  const thumbnailFragment = document.createDocumentFragment();
+  createThumbnails().forEach(({url, description, likes, comments}) => {
+    const templateClone = thumbnailTemplate.cloneNode(true);
+    const templateCloneImg = templateClone.querySelector('.picture__img');
+    templateCloneImg.src = url;
+    templateCloneImg.alt = description;
+    templateClone.querySelector('.picture__likes').textContent = likes;
+    templateClone.querySelector('.picture__comments').textContent = comments.length;
+    thumbnailFragment.append(templateClone);
+  });
+  thumbnailContainer.append(thumbnailFragment);
+}
+
+export {renderThumbnails};
