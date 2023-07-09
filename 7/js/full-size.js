0203aba3cf08e2238d918907119a5e7e3545fe40
Keks <keks@htmlacademy.ru>
2023-07-09 07:19:56 +0300

    :heavy_check_mark: Сборка #7

---
+++ 7/js/full-size.js
@@ -0,0 +1,51 @@
+import {isEscapeKey} from './mocks/util.js';
+
+const fullSizePhoto = document.querySelector('.big-picture');
+const closeButton = fullSizePhoto.querySelector('.big-picture__cancel');
+const commentsList = fullSizePhoto.querySelector('.social__comments');
+const commentItem = commentsList.querySelector('.social__comment');
+
+function onDocumentKeydown(evt) {
+  if (isEscapeKey(evt)) {
+    evt.preventDefault();
+    closeFullSizePhoto();
+  }
+}
+
+function onCloseButtonClick() {
+  closeFullSizePhoto();
+}
+
+function renderComments(comments) {
+  const commentsListFragment = document.createDocumentFragment();
+  comments.forEach(({avatar, name, message}) => {
+    const comment = commentItem.cloneNode(true);
+    const commentPicture = comment.querySelector('.social__picture');
+    commentPicture.src = avatar;
+    commentPicture.alt = name;
+    comment.querySelector('.social__text').innerText = message;
+    commentsList.append(comment);
+  });
+  commentsList.append(commentsListFragment);
+}
+
+function openFullSizePhoto({url, likes, description, comments}) {
+  fullSizePhoto.classList.remove('hidden');
+  document.body.classList.add('overflow-hidden');
+  document.addEventListener('keydown', onDocumentKeydown);
+  closeButton.addEventListener('click', onCloseButtonClick);
+  fullSizePhoto.querySelector('.big-picture__img').src = url;
+  fullSizePhoto.querySelector('.likes-count').textContent = likes;
+  fullSizePhoto.querySelector('.comments-count').textContent = comments.length;
+  renderComments(comments);
+  fullSizePhoto.querySelector('.social__caption').textContent = description;
+}
+
+function closeFullSizePhoto() {
+  fullSizePhoto.classList.add('hidden');
+  document.body.classList.remove('overflow-hidden');
+  document.removeEventListener('keydown', onDocumentKeydown);
+  closeButton.removeEventListener('click', onCloseButtonClick);
+}
+
+export {openFullSizePhoto};
---
+++ 7/js/mocks/util.js
@@ -0,0 +1,47 @@
+/**
+ * Функция для генерации случайного целого числа из заданного диапазона.
+ * @param {number} a - Минимальное или максимальное значение
+ * @param {number} b - Минимальное или максимальное значение
+ * @return {number} - Случайное целое число в диапазоне между a и b
+ */
+function getRandomInteger(a, b) {
+  const lower = Math.ceil(Math.min(a, b));
+  const upper = Math.floor(Math.max(a, b));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+}
+
+/**
+ * Функция для генерации случайного элемента из заданного массива.
+ * @param {Object[]} elements - Массив из которого нужно выбрать элемент
+ * @return {*} - Случайный элемент из массива elements
+ */
+function getRandomArrayElement(elements) {
+  return elements[getRandomInteger(0, elements.length - 1)];
+}
+
+/**
+ * Функция для генерации уникального целого числа.
+ * @param {number} start - Начальное значение, по умолчанию 0
+ * @param {number} increment - Величина, на которую увеличится значение, по умолчанию 1
+ * @return {number} - Число, увеличивающееся на величину increment при каждом вызове функции
+ */
+function getIdGenerator (start = 0, increment = 1) {
+  let lastGeneratedId = start;
+
+  return function () {
+    lastGeneratedId += increment;
+    return lastGeneratedId;
+  };
+}
+
+/**
+ * Функция для проверки, является ли нажатая клавиша Escape.
+ * @param {key} evt - нажатая клавиша
+ * @return {boolean} - если клавиша Escape, возвращает true, иначе false
+ */
+function isEscapeKey(evt) {
+  return evt.key === 'Escape';
+}
+
+export {getRandomInteger, getRandomArrayElement, getIdGenerator, isEscapeKey};
