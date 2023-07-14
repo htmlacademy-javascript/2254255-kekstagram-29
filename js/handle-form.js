fc9da290c336fb3ab1cd45179cce0ff8cebc20a6
Andrey Matveev <andrei_matveev1250@mail.ru>
2023-07-15 04:44:09 +0600

    Правит index.html

---
+++ js/handle-form.js
@@ -0,0 +1,6 @@
+import {isEscapeKey} from './util.js';
+
+const uploadForm = document.querySelector('.img-upload__form');
+const uploadOverlay = uploadForm.querySelector('.img-upload__overlay');
+const uploadInput = uploadForm.querySelector('.img-upload__input');
+const uploadCancel = uploadForm.querySelector('.img-upload__cancel');
